4764dde87b2ed158e5a98e912db003137f9c6260
Shizuka <gcabbage2391@gmail.com>
2026-02-09 17:27:57 +0800

    Created context data function for RecipeListView

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -69,5 +69,9 @@ ctx = {
 }
 
 class RecipeListView(TemplateView):
+    template_name = 'ledger/recipe_list.html'
 
+    def get_context_data(self, **kwargs):
+        return ctx
+        
 class RecipeInfoView(TemplateView):
