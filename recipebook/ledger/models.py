4dfd735874dccba25e128face2e5f91951e9aaa6
Shizuka <gcabbage2391@gmail.com>
2026-02-19 15:38:44 +0800

    Added temporary urls for models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
-from django.db import models
-from django.urls import reverse
-# Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length = 50)
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('tasks:task_detail', args=[str(self.id)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length = 50)
@@ -16,18 +16,19 @@ class Recipe(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('tasks:task_detail', args=[str(self.id)])
+
 class RecipeIngredient(models.Model):
     name = models.CharField(max_length = 50)
     quantity = models.IntegerField()
     corresponding_ingredient = models.ForeignKey(
         Ingredient, 
         on_delete=models.CASCADE,
-        related_name='tasks'
+        related_name='ingredients'
     )
     corresponding_recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='recipes'
+        related_name='recipe'
     )
-    def __str__(self):
-        return self.name
