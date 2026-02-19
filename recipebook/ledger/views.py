b66c2abeed7e91b7ca2b4d31491d5f8eab58b9f0
Shizuka <gcabbage2391@gmail.com>
2026-02-19 16:24:00 +0800

    Configured RecipeInfoView to search RecipeIngredients

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.generic import TemplateView
-from .models import Recipe
+from .models import Recipe, RecipeIngredient
 # Create your views here.
 class RecipeListView(TemplateView):
     template_name = "ledger/recipe_list.html"
@@ -16,5 +16,11 @@ class RecipeInfoView(TemplateView):
     template_name = 'ledger/recipe_info.html'
 
     def get_context_data(self, **kwargs):
-        recipe_id = int(self.kwargs.get('recipe_id')) - 1
-        return ctx['recipes'][recipe_id]
+        ctx = super().get_context_data(**kwargs)
+        recipe_id = int(self.kwargs.get('recipe_id'))
+        recipe_title = Recipe.objects.filter(pk=recipe_id).title
+        ingredients = RecipeIngredient.objects.filter(corresponding_recipe__title=recipe_title)
+
+        ctx['ingredients'] = ingredients
+        ctx['name'] = recipe
+        return ctx
