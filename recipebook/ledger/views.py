dd41cf7925a6fb2cc172bc6b407149c2c27457df
Shizuka <gcabbage2391@gmail.com>
2026-02-19 16:07:34 +0800

    Made RecipeListView take Recipes from database

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,75 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.generic import TemplateView
+from .models import Recipe
 # Create your views here.
-ctx = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quanity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    ]
-}
-
 class RecipeListView(TemplateView):
-    template_name = 'ledger/recipe_list.html'
-
+    recipes = Recipe.objects.all()
+    ctx = {"recipes": recipes}
     def get_context_data(self, **kwargs):
         return ctx
 
