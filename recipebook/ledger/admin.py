93f7fc1270cee878e260e3eb45c3e6d7cb5b92ad
Shizuka <gcabbage2391@gmail.com>
2026-02-26 17:36:51 +0800

    Conformed ledger/admin.py to PEP8

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,12 +1,20 @@
+"""Main admin panel for ledger project."""
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient
 # Register your models here.
 
+
 class RecipeIngredientInline(admin.TabularInline):
+    """Inline class for RecipeIngredient in admin panel."""
+
     model = RecipeIngredient
 
+
 class RecipeAdmin(admin.ModelAdmin):
+    """Settings for how recipes will be displayed in RecipeAdmin."""
+
     model = Recipe
     inlines = [RecipeIngredientInline,]
+
 
 admin.site.register(Recipe, RecipeAdmin)
