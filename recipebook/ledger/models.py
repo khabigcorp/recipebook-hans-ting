c0c453d6b4a9caffbac61d6fbe80eeb76d306061
Shizuka <gcabbage2391@gmail.com>
2026-02-26 17:42:46 +0800

    Conformed ledger/models.py to PEP8

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,29 +1,44 @@
+"""File that stores models of ledger app."""
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+
+
 class Ingredient(models.Model):
-    name = models.CharField(max_length = 50)
-    
+    """Ingredient class that represents recipe ingredients."""
+
+    name = models.CharField(max_length=50)
+
     def __str__(self):
+        """Return string version of Ingredient."""
         return self.name
-    
+
     def get_absolute_url(self):
+        """Get url to Ingredient object."""
         return reverse("ledger:recipe-info", kwargs={"recipe_id": self.pk})
+
 
 class Recipe(models.Model):
-    name = models.CharField(max_length = 50)
-    
+    """Recipe class that represents recipes."""
+
+    name = models.CharField(max_length=50)
+
     def __str__(self):
+        """Return string version of Recipe."""
         return self.name
 
     def get_absolute_url(self):
+        """Get url to Recipe object."""
         return reverse("ledger:recipe-info", kwargs={"recipe_id": self.pk})
 
+
 class RecipeIngredient(models.Model):
-    name = models.CharField(max_length = 50)
+    """Associative object allowing for recipe-ingredient lookup."""
+
+    name = models.CharField(max_length=50)
     quantity = models.IntegerField()
     corresponding_ingredient = models.ForeignKey(
-        Ingredient, 
+        Ingredient,
         on_delete=models.CASCADE,
         related_name='recipes'
     )
