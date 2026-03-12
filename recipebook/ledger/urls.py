6428a54fa68a82a6915a91c32a4027bff3fff165
Shizuka <gcabbage2391@gmail.com>
2026-03-12 16:03:56 +0800

    Added redirect to orignal page for recipe images

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,10 @@
 """File that lists ledger app's valid urls."""
 from django.urls import path
-from .views import RecipeListView, RecipeInfoView, RecipeAddView
+from .views import RecipeListView, RecipeInfoView, RecipeAddView, RecipeAddImageView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
     path('recipe/<int:pk>', RecipeInfoView.as_view(), name='recipe-info'),
-    path('recipe/add', RecipeAddView.as_view(), name='recipe-add')
+    path('recipe/add', RecipeAddView.as_view(), name='recipe-add'),
+    path('recipe/<int:pk>/add_image', RecipeAddImageView.as_view(), name='recipe-add-image'),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,8 +1,9 @@
 """Module that lists views of ledger app."""
 from django.views.generic import TemplateView, CreateView
-from .forms import RecipeForm
+from .forms import RecipeForm, RecipeImageForm
 from .models import Recipe, RecipeIngredient, RecipeImage
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 # Create your views here.
 
 
@@ -42,6 +43,27 @@ class RecipeInfoView(LoginRequiredMixin, TemplateView):
 
 
 class RecipeAddView(LoginRequiredMixin, CreateView):
+    """View for adding recipes."""
     model = Recipe
     form_class = RecipeForm
     template_name = 'ledger/recipe_add.html'
+
+
+class RecipeAddImageView(LoginRequiredMixin, CreateView):
+    "View for adding images to recipes."
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = 'ledger/recipe_add_image.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = RecipeImageForm()
+        context['pk'] = self.kwargs['pk']
+        context['form'] = form
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save()
+            return redirect('ledger:recipe-info', pk=image.corresponding_recipe.pk)
